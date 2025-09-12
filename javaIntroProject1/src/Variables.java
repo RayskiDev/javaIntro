909699c291655526badc4069abf192dfb9b3a502
RayskiDev <mpielka90@gmail.com>
2025-09-12 12:28:00 +0200

    new examples

---
+++ javaIntroProject1/src/Variables.java
@@ -0,0 +1,11 @@
+class Variables {
+    public static void main(String[] args) {
+        String ja = "Slawek";
+        String ty = "Slawek";
+
+        boolean porownanie1 = ja == ty;
+        boolean porownanie2 = ja.equals(ty);
+        System.out.println(porownanie1);
+        System.out.println(porownanie2);
+    }
+}
