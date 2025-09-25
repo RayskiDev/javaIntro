0d50f3135336ca6c92a08b166182ea99bebfc0ac
RayskiDev <mpielka90@gmail.com>
2025-09-25 21:44:24 +0200

    more exercises

---
+++ zadania2/src/sumFromUser.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class sumFromUser {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        scan.useLocale(Locale.US);
+        double userNumber;
+        double userNumberSum = 0;
+
+        do {
+            System.out.print("Podaj liczbę: ");
+            userNumber = scan.nextDouble();
+            userNumberSum += userNumber;
+        } while (userNumber != 0);
+
+        System.out.println("Suma wszystkich wprowadzonych liczb różnych od 0 to: " + userNumberSum);
+    }
+}
