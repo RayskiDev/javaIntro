05838312ad8db2ba06ef5470656b3ac6604b39af
RayskiDev <mpielka90@gmail.com>
2025-10-08 12:27:16 +0200

    new exercises

--- zadania3/src/AdvancedBinaryConverter.java
+++ zadania3/src/AdvancedBinaryConverter.java
@@ -5,26 +5,12 @@ public class AdvancedBinaryConverter {
         Scanner scan = new Scanner(System.in);
         int userNumber;
         int system;
-        int[] bits = new int[32];
+        int[] zmBits = new int[32];
 
         System.out.print("Podaj liczbę całkowitą: ");
         userNumber = scan.nextInt();
         System.out.print("Wybierz system kodowania: ");
         system = scan.nextInt();
-
-        switch (system) {
-            case 1:
-                zmConverter();
-                break;
-            case 2:
-                zu1Converter();
-                break;
-            case 3:
-//                zu2Converter();
-                break;
-            default:
-                System.out.print("Błąd w wyborze systemu!");
-        }
 
 
         int signBit = (userNumber < 0) ? 1 : 0;
@@ -32,20 +18,62 @@ public class AdvancedBinaryConverter {
 
         int index = 0;
         while (index < 32 && absValue > 0) {
-            bits[index] = absValue % 2;
+            zmBits[index] = absValue % 2;
             absValue /= 2;
             index++;
         }
 
+        int closestPower = closestPower(index);
 
-        static void zmConverter() {
-            if (userNumber < 0) {
-                for (int i = 0; i < index; i++) {
-                    if (bits[i] == 1) bits[i] = 0;
-                    else bits[i] = 1;
-//                    zu1[i] = (zmBits[i] == 0) ? 1 : 0;
+
+        switch (system) {
+            case 1:
+                writeArray(closestPower, userNumber, zmBits, "zm", signBit);
+                break;
+            case 2:
+                int[] zu1Bits = zu1Converter(closestPower, index, signBit, zmBits);
+                writeArray(closestPower, userNumber, zu1Bits, "zu1", signBit);
+                break;
+            case 3:
+//                zu2Converter();
+                break;
+            default:
+                System.out.print("Błąd w wyborze systemu!");
+        }
+    }
+
+    private static void writeArray(int closestPower, int userNumber, int[] zu1Array, String system, int signBit) {
+        System.out.printf("Liczba %d binarnie w systemie %s: %d", userNumber, system, signBit);
+        for (int i = closestPower-2; i >= 0; i--) {
+            System.out.print(zu1Array[i]);
+        }
+    }
+
+    private static int closestPower(int n) {
+        double log2 = Math.log(n) / Math.log(2);
+        int exponent = (int) Math.ceil(log2);
+        return (int) Math.pow(2, exponent);
+    }
+
+    private static int[] zu1Converter(int closestPower, int index, int sign, int[] zmBits) {
+        int[] zu1 = new int[closestPower];
+        if (sign == 1) {
+            for (int i = 0; i < closestPower; i++) {
+                if (i >= index) {
+                    zu1[i] = 1;
+                    continue;
                 }
+                zu1[i] = (zmBits[i] == 0) ? 1 : 0;
+            }
+        } else {
+            for (int i = 0; i < closestPower; i++) {
+                if (i >= index) {
+                    zu1[i] = 0;
+                    continue;
+                }
+                zu1[i] = (zmBits[i] == 0) ? 1 : 0;
             }
         }
+        return zu1;
     }
 }
