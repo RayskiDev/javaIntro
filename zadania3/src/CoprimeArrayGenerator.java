980c5b32084cd88f9111bfb326e7fb5ae1628427
RayskiDev <mpielka90@gmail.com>
2025-10-17 12:29:08 +0200

    exercises 3

--- zadania3/src/CoprimeArrayGenerator.java
+++ zadania3/src/CoprimeArrayGenerator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CoprimeArrayGenerator {
@@ -8,22 +10,47 @@ public class CoprimeArrayGenerator {
 
         boolean[][] dataCube = new boolean[userNumber][userNumber];
 
+
         for (int i = 0; i < userNumber; i++) {
             for (int j = 0; j < userNumber; j++) {
-                if (i == 0 || j == 0) {
-                    dataCube[i][j] = true;
-                    continue;
-                }
-                if ((i+1) % (j+1) != 0) dataCube[i][j] = true;
-                if ((j+1) % (i+1) != 0) dataCube[i][j] = true;
+                dataCube[i][j] = areCoprime(i+1, j+1);
             }
         }
 
+        System.out.print("    ");
+        for (int i = 1; i <= userNumber; i++) {
+            System.out.printf("%3d ", i);
+        }
+        System.out.println();
+
         for (int i = 0; i < userNumber; i++) {
+            System.out.printf("%3d ", i+1);
             for (int j = 0; j < userNumber; j++) {
                 System.out.printf("%3c ", (dataCube[i][j]) ? '+' : '.');
             }
             System.out.println();
         }
+    }
+
+    public static boolean areCoprime(int a, int b) {
+        List<Integer> divisorsA = getDivisors(a);
+        List<Integer> divisorsB = getDivisors(b);
+
+        for (int dA : divisorsA) {
+            if (dA > 1 && divisorsB.contains(dA)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<Integer> getDivisors(int n) {
+        List<Integer> divisors = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+            }
+        }
+        return divisors;
     }
 }
